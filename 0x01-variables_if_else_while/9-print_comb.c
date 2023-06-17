028cb84c4c80a815baeb762e8343f23e1c6aba8f
salimajj03 <salimafinisher@gmail.com>
2023-06-16 21:42:18 -0700

    all single num

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+		putchar('\n');
+
+		return (0);
+}
