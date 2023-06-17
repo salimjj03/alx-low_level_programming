7bf28433728dabf91fc0483289a68247cf30a5e7
salimajj03 <salimafinisher@gmail.com>
2023-06-16 21:53:20 -0700

    all single num

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,14 @@ int main(void)
 {
 	int n;
 
-	for (n = '0'; n <= '9'; n++)
+	for (n = '0'; n <= '9'; ++n)
 	{
 		putchar(n);
+	if (n != '9')
+	{
 		putchar(',');
 		putchar(' ');
+	}
 	}
 		putchar('\n');
 
