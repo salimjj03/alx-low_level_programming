2992c22ef6472fd3160d84ebefcadaf71d36749a
salimajj03 <salimafinisher@gmail.com>
2023-06-16 21:15:49 -0700

    alpha in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 'z'; a >= 'a'; a--)
+	{
+		putchar(a);
+	}
+		putchar('\n');
+
+		return (0);
+}
