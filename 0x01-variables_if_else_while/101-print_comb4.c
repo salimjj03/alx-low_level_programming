e8b58750177bd9e4ce68e1059a68401e594b997f
salimajj03 <salimafinisher@gmail.com>
2023-06-18 03:51:11 -0700

    ccombination of three char

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 1; a <= 9; a++)
+	{
+	for (b = a + 1; b <= 9; b++)
+	{
+	for (c = b + 1; c <= 9; c++)
+	{
+		putchar(a + '0');
+		putchar(b + '0');
+		putchar(c + '0');
+	if (a != 7)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	}
+	}
+		putchar('\n');
+		return (0);
+}
