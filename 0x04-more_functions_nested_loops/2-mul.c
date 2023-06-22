59aba906b5cb887befcaec35899bdfc9094bdd89
salimajj03 <salimafinisher@gmail.com>
2023-06-22 14:26:42 -0700

    func that multipilie two inegers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multipilie two integers
+ * @a: first int
+ * @b: second int
+ * Return: return the result
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
