f04ca8dcf637411a1ac93d7543895c15218c02e9
salimajj03 <salimafinisher@gmail.com>
2023-06-28 11:53:59 -0700

    copy string of pointer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy pointer to another pointer
+ * @dest: fist pointer
+ * @src: second pointer
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+		return (dest);
+}
