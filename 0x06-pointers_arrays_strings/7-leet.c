0e41300f31de00839a269b5d1ec2e832a4fb8670
salimajj03 <salimafinisher@gmail.com>
2023-06-30 15:37:17 -0700

    encode a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,10 +1,25 @@
 #include "main.h"
 
+/**
+ * leet - encode a string
+ * @s: the string
+ * Return: s
+ */
 
 char *leet(char *s)
 {
-	int i;
+	int  i, j;
+	char c[11] = "aAeEoOtTlL", n[11] = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-
+	for (j = 0; c[j] != '\0'; j++)
+	{
+	if (s[i] == c[j])
+	{
+		s[i] = n[j];
+	}
+	}
+	}
+		return (s);
+}
