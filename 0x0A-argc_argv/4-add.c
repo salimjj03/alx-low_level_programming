f154d77cd9d97ee402c125cd8066dfe26738bb85
salimajj03 <salimafinisher@gmail.com>
2023-07-09 08:14:03 -0700

    main agument to find sum

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print name of the program
+ * @argc: count argv
+ * @argv: the array
+ * Return: argv
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+	if (*argv[i] >= 48 && *argv[i] <= 57)
+	{
+		sum = sum + atoi(argv[i]);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	}
+	printf("%d\n", sum);
+		return (0);
+}
