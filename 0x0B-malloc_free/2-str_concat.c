1afc52723fdb9123179f959d30bbd99e6f2d8b69
salimajj03 <salimafinisher@gmail.com>
2023-07-10 14:21:27 -0700

    concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -31,7 +31,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		len2 = len2 + 1;
 	}
-	p = malloc((tlen + 1) * (sizeof(char)));
+	p = malloc((tlen + len2) * (sizeof(char)));
 	if  (p == NULL)
 	{
 		return (NULL);
