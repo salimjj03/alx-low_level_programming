743adcc50f374102272db5979fc8409bc141d60e
salimajj03 <salimafinisher@gmail.com>
2023-07-13 17:14:26 -0700

    calloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -32,9 +32,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (ptr == NULL)
 	{
-		p = malloc(new_size);
-		free(ptr);
-		return (p);
+		return (NULL);
 	}
 	if (new_size == 0 &&  ptr != NULL)
 	{
