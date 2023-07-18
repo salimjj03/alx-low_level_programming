44d2c141017d44230849ec532bbc5f797786accd
salimajj03 <salimafinisher@gmail.com>
2023-07-18 04:00:16 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,42 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - find the length of a string
+ * @s: string to be count
+ * Return: return l
+ */
+
+int _strlen(char *s)
+{
+	 int l = 0;
+
+	for ( ; *s != '\0'; s++)
+	{
+		l += 1;
+	}
+	return (l);
+}
+
+/**
+ * _strcpy - copy pointer to another pointer
+ * @dest: fist pointer
+ * @src: second pointer
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+		return (dest);
+}
+
+/**
   * new_dog - create new dog
   * @name: name of the dog
   * @age:age of the dog
@@ -20,9 +56,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	p->name = name;
+	p->name  = malloc(_strlen(name + 1));
+	if (p->name == NULL)
+	{
+		free(p->name);
+	}
+	_strcpy(p->name, name);
+	p->owner = malloc(_strlen(owner + 1));
+	if (p->owner == NULL)
+	{
+		free(p->owner);
+		free(p);
+	}
+	_strcpy(p->owner, owner);
 	p->age = age;
-	p->owner = owner;
 	return (p);
 }
-
