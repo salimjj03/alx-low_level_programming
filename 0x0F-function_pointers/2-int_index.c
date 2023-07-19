bdb9828d74945d85a50bc4369f91622638d80df1
salimajj03 <salimafinisher@gmail.com>
2023-07-19 08:49:39 -0700

    search int

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,11 +18,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
+	if (array != NULL && cmp != NULL)
+	{
 	for (i = 0; i < size; i++)
 	{
 	if (cmp(array[i]))
 	{
 		return (i);
+	}
 	}
 	}
 		return (-1);
