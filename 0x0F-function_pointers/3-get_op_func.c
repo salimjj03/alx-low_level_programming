10795e8a6c3e24b5b5b938dcda47571ee4ddb811
salimajj03 <salimafinisher@gmail.com>
2023-07-20 05:34:37 -0700

    performs simple operations.

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * get_op_func - ...
+  * @s: ...
+  *
+  * Return: ...
+  */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (i < 5)
+	{
+	if (*(ops[i].op) == *s)
+	{
+		return (ops[i].f);
+	}
+		i++;
+	}
+		return (0);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  * main - perform simple operation
+  * @argc: size of array
+  * @argv: the array
+  *
+  * Return: int
+  */
+
+int main(int argc, char *argv[])
+{
+	int calc, num1, num2;
+	int (*operator)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	operator = get_op_func(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if (operator == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	calc = operator(num1, num2);
+	printf("%d\n", calc);
+	return (0);
+}
