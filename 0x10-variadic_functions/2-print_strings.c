90a0d99fd79f3c31318ce56f07d7dfae9a61b8b0
salimajj03 <salimafinisher@gmail.com>
2023-07-20 17:52:48 -0700

    print string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,41 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+  * print_strings - print numbers
+  * @separator: string
+  * @n:number of agument
+  *
+  * Return: void
+  */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list argc;
+	unsigned int i;
+	char *s;
+
+	va_start(argc, n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(argc, char*);
+	if (separator == NULL)
+	{
+		printf("%s", s);
+	}
+	else
+	{
+	if (s == NULL)
+	{
+		printf("(nil)");
+	}
+		printf("%s", s);
+	if (i < n - 1)
+	{
+		printf("%s", separator);
+	}
+	}
+	}
+		printf("\n");
+}
