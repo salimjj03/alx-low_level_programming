5fc5a1577f5b43b36b02f40a62b277d1a37f50e4
salimajj03 <salimafinisher@gmail.com>
2023-07-23 09:04:53 -0700

    print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,87 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * i - ...
+ * @argc:..
+ * Return: ...
+ */
+
+void i(va_list argc)
+{
+	printf("%d", va_arg(argc, int));
+}
+
+/**
+ * c - ..
+ * @argc:..
+ * Return: ...
+ */
+
+void c(va_list argc)
+{
+	printf("%c", va_arg(argc, int));
+}
+
+/**
+ * f - ...
+ * @argc: ..
+ * Return:...
+ */
+
+void f(va_list argc)
+{
+	printf("%f", va_arg(argc, double));
+}
+
+/**
+ * s - ..
+ * @argc: ..
+ * Return: ..
+ */
+
+void s(va_list argc)
+{
+if (argc == NULL)
+{
+	printf("(nil)");
+}
+	printf("%s", va_arg(argc, char*));
+}
+
+/**
+ * print_all - ..
+ * @format: ...
+ * Return:...
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list argc;
+	int x, y;
+	char fmt[] = "icfs";
+
+	void (*ptr[])(va_list argc) = {i, c, f, s};
+
+	va_start(argc, format);
+	x = 0;
+	while (format[x] != '\0')
+	{
+	y = 0;
+	while (fmt[y] != '\0')
+	{
+	if (format[x] == fmt[y])
+	{
+		ptr[y](argc);
+	if (format[x + 1] != '\0')
+	{
+		printf(", ");
+	}
+	}
+		y++;
+	}
+		x++;
+	}
+		printf("\n");
+}
