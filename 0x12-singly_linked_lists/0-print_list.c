adb318d78086c3bdec5c700c1b133d20f1befa2d
salimajj03 <salimafinisher@gmail.com>
2023-07-27 16:41:39 -0700

    all the elements of linked

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_list - ....
+ * @h: start of the node
+ * Return: ....
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *temp;
+	int i = 0;
+
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)");
+	}
+	else
+	{
+	temp = h;
+	while (temp != NULL)
+	{
+		printf("[%d] %s\n", temp->len, temp->str);
+		temp = temp->next;
+		i++;
+	}
+	}
+		return (i);
+}
