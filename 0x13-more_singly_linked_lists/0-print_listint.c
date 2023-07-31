6243789c0c81dc908270de1c58ff4e8d05b156c4
salimajj03 <salimafinisher@gmail.com>
2023-07-31 04:11:20 -0700

    print all node

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - print all node
+ * @h: the list
+ * Return: number of node
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t n_node = 0;
+	const listint_t *tmp;
+
+	tmp = h;
+	while (tmp->next != 0)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+		n_node++;
+	}
+		printf("%d\n", tmp->n);
+		n_node++;
+		return (n_node);
+}
