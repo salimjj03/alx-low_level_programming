101e0adceba4677df0c6858b370edab2e362ea05
salimajj03 <salimafinisher@gmail.com>
2023-07-31 04:21:54 -0700

    print number of node

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - print number of node
+ * @h: the list
+ * Return: number of node
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t n_node = 0;
+	const listint_t *tmp;
+
+	tmp = h;
+	while (tmp->next != 0)
+	{
+		tmp = tmp->next;
+		n_node++;
+	}
+		n_node++;
+		return (n_node);
+}
