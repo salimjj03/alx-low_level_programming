dc86d14c94859ff61bd28175150b60d006599f2e
salimajj03 <salimafinisher@gmail.com>
2023-07-31 13:20:22 -0700

    delete first node in list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+  * pop_listint - ...
+  * @head: ...
+  *
+  * Return: ...
+  */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int n;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+	tmp = *head;
+	*head = tmp->next;
+	n = tmp->n;
+	free(tmp);
+	return (n);
+}
