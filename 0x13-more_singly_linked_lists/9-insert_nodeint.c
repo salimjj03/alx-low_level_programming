bd4d1f64291b860fe1556b6c7fb7666cb9c85f88
salimajj03 <salimafinisher@gmail.com>
2023-08-01 07:31:57 -0700

    add node at n position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		newnode->next = tmp->next;
 		tmp->next = newnode;
-		return (tmp);
+		return (newnode);
 	}
 		tmp = tmp->next;
 		i++;
