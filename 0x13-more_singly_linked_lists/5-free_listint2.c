5d74ab89164146ab5adeaa1f6564c18020b38023
salimajj03 <salimafinisher@gmail.com>
2023-08-01 06:00:30 -0700

    print head and set it to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,9 +12,9 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head != NULL)
+	if (*head)
 	{
-	while (*head != NULL)
+	while (*head)
 	{
 		temp = *head;
 		*head = (*head)->next;
