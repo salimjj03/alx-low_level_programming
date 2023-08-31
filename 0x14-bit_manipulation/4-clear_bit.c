9e39a17b043be7503c3f6f84b483cd9e6d5153db
salim aliyu musawa <salemaliyu1992@gmail.com>
2023-08-31 16:15:26 -0700

    clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,12 +12,10 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	int bit;
-
-	if (index < sizeof(unsigned long int) * 8)
+	if (index > sizeof(unsigned long int) * 8)
 	{
-	bit = *n &= (~(1 << index));
-	return (bit);
+		return (-1);
 	}
-	return (-1);
+	*n &= (~(1 << index));
+	return (1);
 }
