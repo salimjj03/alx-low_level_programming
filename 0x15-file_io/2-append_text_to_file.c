d4f1767aef9008c43540f8c79d6ff37db055fc35
salim aliyu musawa <salemaliyu1992@gmail.com>
2023-09-04 15:43:40 -0700

    appand a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -34,7 +34,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	{
-		return (0);
+		return (-1);
 	}
 	file_d = open(filename, O_RDWR | O_APPEND);
 	if (file_d == -1)
