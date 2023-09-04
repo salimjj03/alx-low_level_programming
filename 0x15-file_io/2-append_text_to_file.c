47c0f87ae19bbae1945e6b691d35e7bb3132751b
salim aliyu musawa <salemaliyu1992@gmail.com>
2023-09-04 15:40:08 -0700

    appand a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * _strlen - find the length of a string
+ * @s: string to be count
+ * Return: return l
+ */
+
+int _strlen(char *s)
+{
+	 int l = 0;
+
+	for ( ; *s != '\0'; s++)
+	{
+		l += 1;
+	}
+	return (l);
+}
+
+/**
+  * append_text_to_file - ...
+  * @filename: ...
+  * @text_content: ...
+  *
+  * Return: ...
+  */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file_d;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	file_d = open(filename, O_RDWR | O_APPEND);
+	if (file_d == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		if (write(file_d, text_content, _strlen(text_content)) == -1)
+		{
+			return (-1);
+		}
+	}
+	return (1);
+}
