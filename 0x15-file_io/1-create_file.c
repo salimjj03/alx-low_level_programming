14b52c44776d3db1af3c5aaaf7676991fde32b71
salim aliyu musawa <salemaliyu1992@gmail.com>
2023-09-04 15:05:04 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -41,10 +41,13 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	if (text_content != NULL)
+	{
 	if (write(file_d, text_content, _strlen(text_content)) == -1)
 	{
 		close(file_d);
 		return (-1);
+	}
 	}
 	close(file_d);
 	return (1);
