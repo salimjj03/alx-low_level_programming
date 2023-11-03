26becf19c4ca97a328edaec77a79d54132492e5f
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-11-03 14:45:32 -0700

    get element

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - ...
+ * @ht: ..
+ * @key: .....
+ * Return: ....
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+
+	if (ht == NULL)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+	if (strcmp(key, ht->array[index]->key) == 0)
+		return (ht->array[index]->value);
+	return (NULL);
+}
